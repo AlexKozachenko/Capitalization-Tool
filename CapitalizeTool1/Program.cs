d145d82f70a82cd3214ece0faeefa8d206fd4281
AlexKozachenko <kozachenko@gmail.com>
2018-04-22 18:33:43 +0300

    change

--- CapitalizeTool1/Program.cs
+++ CapitalizeTool1/Program.cs
@@ -1,14 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
-namespace CapitalizeTool1
+namespace ConsoleApplication11
 {
-    class Program
+    class CapitalizeTool
     {
-        static void Main(string[] args)
+        private Char[] PunctuationMarks = { ';', ':', '.', ',', '!', '?', '-' };
+        private String[] ShortWords = { "A", "An", "And", "At", "But", "By", "For", "In", "Into", "Nor", "Not",
+            "Of", "On", "Or", "Out", "So", "The", "To", "Up", "Yet"};
+        private Boolean IsShortWords(String Line)
         {
+            Boolean flag = false;
+            for (UInt16 i = 0; i < ShortWords.Length; i++)
+            {
+                if (Line == ShortWords[i])
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+        }
+        private Boolean IsPunctuationMarks(Char Mark)
+        {
+            Boolean flag = false;
+            for (UInt16 i = 0; i < PunctuationMarks.Length; i++)
+            {
+                if (Mark == PunctuationMarks[i])
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+        }
+        public void Capitalize()
+        {
+            Console.WriteLine("Enter string:");
+            UInt16 TopCursorPosition = 0;
+            String Line;
+            do
+            {
+                TopCursorPosition++;
+                if (TopCursorPosition > 1)
+                {
+                    Console.WriteLine("Enter string:");
+                    TopCursorPosition++;
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                do
+                {
+                    Line = Console.ReadLine();
+                    if (Line == "")
+                    {
+                        Console.SetCursorPosition(0, TopCursorPosition);
+                    }
+                    Line = Line.ToLower();
+                    String[] Line1 = Line.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    for (UInt16 i = 0; i < Line1.Length; i++)
+                    {
+                        Char ch = Char.ToUpper(Line1[i][0]);
+                        Line1[i] = Line1[i].Remove(0, 1);
+                        Line1[i] = Line1[i].Insert(0, new String(ch, 1));
+                        if (i != 0 && i != Line1.Length - 1 && IsShortWords(Line1[i]) == true)
+                        {
+                            Line1[i] = Line1[i].ToLower();
+                        }
+                    }
+                    Line = String.Join(" ", Line1);
+                    for (UInt16 i = 1; i < Line.Length; i++)
+                    {
+                        if (IsPunctuationMarks(Line[i]) == true && Line[i - 1] == ' ')
+                        {
+                            Line = Line.Remove(i - 1, 1);
+                        }
+                    }
+                    for (UInt16 i = 0; i < Line.Length - 1; i++)
+                    {
+                        if (IsPunctuationMarks(Line[i]) == true && Line[i + 1] != ' ')
+                        {
+                            Line = Line.Insert(i + 1, " ");
+                        }
+                    }
+                    for (UInt16 i = 0; i < Line.Length; i++)
+                    {
+                        if ((Line[i] == '-') && Line[i - 1] != ' ')
+                        {
+                            i--;
+                            Line = Line.Insert(i + 1, " ");
+                        }
+                    }
+                }
+                while (Line == "");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(Line);
+                Console.ResetColor();
+                TopCursorPosition++;
+            }
+            while (true);
+        }
+        public static void Main()
+        {
+            CapitalizeTool CapitalizeTool = new CapitalizeTool();
+            CapitalizeTool.Capitalize();
         }
     }
 }
