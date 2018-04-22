de71ca9f824b5a6a1359357b08fdc9831e8d7579
AlexKozachenko <kozachenko@gmail.com>
2018-04-22 23:41:15 +0300

    change

--- CapitalizeTool1/Program.cs
+++ CapitalizeTool1/Program.cs
@@ -7,14 +7,14 @@ namespace CapitalizationTool
     class CapitalizationTool
     {
         private Char[] PunctuationMarks = { ';', ':', '.', ',', '!', '?', '-' };
-        private String[] ShortWords = { "A", "An", "And", "At", "But", "By", "For", "In", "Nor", "Not",
+        private String[] EmptyWords = { "A", "An", "And", "At", "But", "By", "For", "In", "Nor", "Not",
             "Of", "On", "Or", "Out", "So", "The", "To", "Up", "Yet"};
-        private Boolean IsShortWords(String Line)
+        private Boolean IsEmptyWords(String Line)
         {
             Boolean flag = false;
-            for (UInt16 i = 0; i < ShortWords.Length; i++)
+            for (UInt16 i = 0; i < EmptyWords.Length; i++)
             {
-                if (Line == ShortWords[i])
+                if (Line == EmptyWords[i])
                 {
                     flag = true;
                     break;
@@ -37,7 +37,7 @@ namespace CapitalizationTool
         }
         public void Capitalize()
         {
-            Console.WriteLine("Original title:");
+            Console.WriteLine("Enter title to capitalize: ");
             UInt16 TopCursorPosition = 0;
             String Line;
             do
@@ -45,75 +45,93 @@ namespace CapitalizationTool
                 TopCursorPosition++;
                 if (TopCursorPosition > 1)
                 {
-                    Console.WriteLine("Original title:");
-                    TopCursorPosition++;
+                    Console.WriteLine("Enter title to capitalize: ");
+                    Console.SetCursorPosition("Enter title to capitalize: ".Length, TopCursorPosition);
                 }
                 Console.ForegroundColor = ConsoleColor.Red;
                 do
                 {
+                    Console.SetCursorPosition("Enter title to capitalize: ".Length, TopCursorPosition - 1);
                     Line = Console.ReadLine();
-                    if (Line == "")
+                    //если нажат Enter, курсор остается на месте
+                    if (Line.Length == 0)
                     {
                         Console.SetCursorPosition(0, TopCursorPosition);
                     }
-                    Line = Line.ToLower();
-                    for (UInt16 i = 0; i < Line.Length; i++)
+                    else
                     {
-                        if (IsPunctuationMarks(Line[i]) == true)
+                        Line = Line.ToLower();
+                        //если по ходу строки встречаются знаки препинания, отделяем их пробелами
+                        for (UInt16 i = 0; i < Line.Length; i++)
                         {
-                            i--;
-                            Line = Line.Insert(i + 1, " ");
-                            Line = Line.Insert(i + 3, " ");
-                            i += 4;
+                            if (IsPunctuationMarks(Line[i]) == true)
+                            {
+                                i--;
+                                Line = Line.Insert(i + 1, " ");
+                                Line = Line.Insert(i + 3, " ");
+                                i += 4;
+                            }
                         }
-                    }
-                    String[] Line1 = Line.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    for (UInt16 i = 0; i < Line1.Length; i++)
-                    {
-                        Char FirstLetter = Char.ToUpper(Line1[i][0]);
-                        Line1[i] = Line1[i].Remove(0, 1);
-                        Line1[i] = Line1[i].Insert(0, new String(FirstLetter, 1));
-                        if (i != 0 && i != Line1.Length - 1 && IsShortWords(Line1[i]) == true)
+                        //делаем из строки массив подстрок
+                            String[] Line1 = Line.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        //поднимаем первые буквы подстрок
+                        for (UInt16 i = 0; i < Line1.Length; i++)
                         {
-                            Line1[i] = Line1[i].ToLower();
+                            Char FirstLetter = Char.ToUpper(Line1[i][0]);
+                            Line1[i] = Line1[i].Remove(0, 1);
+                            Line1[i] = Line1[i].Insert(0, new String(FirstLetter, 1));
+                            //если текущее слово не первое и не последнее и является вспомогательным, опускаем первую букву
+                            if (i != 0 && i != Line1.Length - 1 && IsEmptyWords(Line1[i]) == true)
+                            {
+                                Line1[i] = Line1[i].ToLower();
+                            }
                         }
-                    }
-                    if (Line1.Length > 0 &&IsPunctuationMarks(Line1[Line1.Length - 1][0]) == true)
-                    {
-                        Char FirstLetter = Char.ToUpper(Line1[Line1.Length - 2][0]);
-                        Line1[Line1.Length - 2] = Line1[Line1.Length - 2].Remove(0, 1);
-                        Line1[Line1.Length - 2] = Line1[Line1.Length - 2].Insert(0, new String(FirstLetter, 1));
-                    }
-                    Line = String.Join(" ", Line1);
-                    for (UInt16 i = 1; i < Line.Length; i++)
-                    {
-                        if (IsPunctuationMarks(Line[i]) == true && Line[i - 1] == ' ')
+                        //если в конце строки знак препинания, поднимаем первую букву слова перед ним
+                        if (Line1.Length > 0 && IsPunctuationMarks(Line1[Line1.Length - 1][0]) == true)
                         {
-                            Line = Line.Remove(i - 1, 1);
+                            Char FirstLetter = Char.ToUpper(Line1[Line1.Length - 2][0]);
+                            Line1[Line1.Length - 2] = Line1[Line1.Length - 2].Remove(0, 1);
+                            Line1[Line1.Length - 2] = Line1[Line1.Length - 2].Insert(0, new String(FirstLetter, 1));
                         }
-                    }
-                    for (UInt16 i = 0; i < Line.Length - 1; i++)
-                    {
-                        if (IsPunctuationMarks(Line[i]) == true && Line[i + 1] != ' ')
+                        //собираем строку
+                        Line = String.Join(" ", Line1);
+                        //если была введена пробельная строка, StringSplitOptions.RemoveEmptyEntries сделает ее пустой и она не будет обрабатываться, поэтому добавляем пробел
+                        if (Line.Length == 0)
                         {
-                            Line = Line.Insert(i + 1, " ");
+                            Line = Line.Insert(0, " ");
                         }
-                    }
-                    for (UInt16 i = 0; i < Line.Length; i++)
-                    {
-                        if (Line[i] == '-' && Line[i - 1] != ' ')
+                        //убираем пробел перед знаком препинания
+                        for (UInt16 i = 1; i < Line.Length; i++)
                         {
-                            i--;
-                            Line = Line.Insert(i + 1, " ");
+                            if (IsPunctuationMarks(Line[i]) == true && Line[i - 1] == ' ')
+                            {
+                                Line = Line.Remove(i - 1, 1);
+                            }
+                        }
+                        //вставляем пробел после знака препинания, если его не было
+                        for (UInt16 i = 0; i < Line.Length - 1; i++)
+                        {
+                            if (IsPunctuationMarks(Line[i]) == true && Line[i + 1] != ' ')
+                            {
+                                Line = Line.Insert(i + 1, " ");
+                            }
+                        }
+                        //форматируем тире
+                        for (UInt16 i = 0; i < Line.Length; i++)
+                        {
+                            if (Line[i] == '-' && Line[i - 1] != ' ')
+                            {
+                                i--;
+                                Line = Line.Insert(i + 1, " ");
+                            }
                         }
                     }
                 }
-                while (Line == "");
-               
-                //Console.ResetColor();
-                //Console.WriteLine("Capitalized title: ");
+                while (Line.Length == 0);
+                Console.ResetColor();
+                Console.WriteLine("Capitalized title: ");
+               Console.SetCursorPosition("Capitalized title: ".Length, TopCursorPosition); 
                 Console.ForegroundColor = ConsoleColor.Green;
-               
                 Console.WriteLine(Line);
                 Console.ResetColor();
                 TopCursorPosition++;
